38374d62ef099f23deb9f82572f6f0365e1621f9
jacqueline R <jacqueline.raynaud@gmail.com>
2026-02-19 10:00:26 +0100

    04 activity 3 - accessibility - message error - element size

--- app/src/main/java/fr/opc/practice/p9a11y/Case3Activity.kt
+++ app/src/main/java/fr/opc/practice/p9a11y/Case3Activity.kt
@@ -2,29 +2,48 @@ package fr.opc.practice.p9a11y
 
 import android.content.res.ColorStateList
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.doOnTextChanged
 import fr.opc.practice.p9a11y.databinding.ActivityCase3Binding
 
+
 class Case3Activity : AppCompatActivity() {
     private lateinit var binding: ActivityCase3Binding
 
+    // --- OnCreate ---
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         binding = ActivityCase3Binding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
 
+        //--- error message with validation
+        binding.validateButton.setOnClickListener {
+            val textLength = binding.pseudoEdit.text?.length ?: 0
+            if (textLength < 3) {
+                val message = getString(R.string.cas_3_pseudo_court, textLength)
+                // for user
+                binding.errorText.text = message
+                binding.errorText.visibility = View.VISIBLE
+                // for accessibility talkback
+                binding.validateButton.announceForAccessibility(message)
+            }
+        }
+        // --- validation on text change
         binding.pseudoEdit.doOnTextChanged { text, _, _, _ ->
-            text?.length?.let { textLength ->
-                binding.validateButton.isEnabled = textLength > 2
+            val textLength = text?.length ?: 0
+            binding.errorText.visibility = View.INVISIBLE
+            if (textLength < 3) {
                 binding.pseudoEdit.backgroundTintList = if (textLength > 2) {
                     ColorStateList.valueOf(resources.getColor(R.color.green400, theme))
                 } else {
                     ColorStateList.valueOf(resources.getColor(R.color.red400, theme))
                 }
             }
+
         }
-        binding.validateButton.isEnabled = false
     }
 }
+
